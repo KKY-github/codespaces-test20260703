fdcefbc24c759cb6c123ec48497d9abd47bec3e6
Yongveloper <beadkco@naver.com>
2026-07-03 06:11:25 +0000

    Render에 Deploy하기 위해서 PORT 변수 변경

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // JSON 요청 본문(body)을 파싱하기 위한 미들웨어
 app.use(express.json());
